df815511e3421355fd17427a0a14063d11eda7de
Nuno Martins <nuno@alvesmartins.eu>
2020-02-29 15:49:40 +0000

    Create first draft of the homepage

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,10 @@
 import "bootstrap";
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWx2ZXNtYXJ0aW5zIiwiYSI6ImNrNHprcjZ3ZzBiYzQzam9kYmNmcm9yd2wifQ.5NITBJndEqiHl2Vm8tVPDg';
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9',
+    center: [-9.142685,38.736946],
+    zoom: 13
+});
