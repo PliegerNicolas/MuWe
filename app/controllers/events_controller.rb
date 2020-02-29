f28f2ff2542a1f0c67f8594c447203fa896c3fca
Nicolas Plieger <plieger05@gmail.com>
2020-02-29 23:43:05 +0000

    Creating the events controller

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,40 @@
+class EventsController < ApplicationController
+  def index
+    @events = policy_scope(Event)
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(
+      :user_id, :city,
+      :address, :longitude, :latitude,
+      :description, :music_style,
+      :number_of_max_players,
+      :date, :status
+    )
+  end
+
+  def set_event
+    @event = Event.find(params[:id])
+    authorize @event
+  end
+end
