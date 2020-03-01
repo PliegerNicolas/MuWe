a41b4d130f3dcdac103a7ae9fa25dccc52a20e48
Nicolas Plieger <plieger05@gmail.com>
2020-03-01 22:37:57 +0000

    Adding profiles controller and public profile page

--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'events#index'
-  get 'jams', to: 'events#jams'
-  get 'prospect', to: 'posts#prospect'
+  get 'jams', to: 'events#jams' # Main jams page similar to index
+  get 'prospect', to: 'posts#prospect' # Main prospect page
+
+  get 'profile/:id', to: 'profiles#profile', as: :profile # current_user profile page
 
   resources :events
 end
-
-#  resources :events do
-#    collection do
-#      get 'jams'
-#    end
-#  end
-
-#  resources :posts do
-#    collection do
-#      get 'prospect'
-#    end
-#  end
