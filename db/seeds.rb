43b02b2f4b1d15e466366b8d5aa206cb3576735e
Nicolas Plieger <plieger05@gmail.com>
2020-03-01 01:56:55 +0000

    Adding beginning of seed and correcting space in user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,23 +6,69 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+# Cleaning DB
+
 puts 'Destroying all Events and Participants'
 Event.destroy_all
 
 puts 'Destroying all Users, Profiles and Participants again'
-User.destroy_all
+User.destroy_all # if Rails.env.development?
 
 puts "Destroying Comments, Ratings, Followers and posts"
-Comment.destroy_all
-Rating.destroy_all
-Follower.destroy_all
-Post.destroy_all
+Comment.destroy_all # if Rails.env.development?
+Rating.destroy_all # if Rails.env.development?
+Follower.destroy_all # if Rails.env.development?
+Post.destroy_all # if Rails.env.development?
 
-print 'Creating Users'
-10.times do
+puts "Destroying all instruments"
+Instrument.destroy_all
+
+#End Cleaning DB
+
+# Creating users
+
+puts "Creating custom development accounts"
+
+['Nuno', 'Sofia', 'Nicolas'].each do |name|
   new_user = User.create!(
-    first_name: Faker::Name.first_name
-    email: Faker::Internet.email
+    first_name: "#{name}",
+    email: "#{name}@gmail.com",
+    password: '123456'
+    )
+puts "Creating custom test account for #{new_user.first_name} (email = #{new_user.email}, password = 123456 })"
+print 'Creating random Users'
+end
+print ' { '
+7.times do
+  print '#'
+  new_user = User.create!(
+    first_name: Faker::Name.first_name,
+    email: Faker::Internet.email,
     password: '123456'
     )
 end
+print ' }'
+
+# End creating users
+
+puts ''
+
+# Creating instruments
+
+print 'Creating instruments'
+print ' { '
+arr = []
+url = 'https://simple.wikipedia.org/wiki/List_of_musical_instruments'
+document = Nokogiri::HTML.parse(open(url))
+document.css('.mw-parser-output').each do |instrument_list|
+  instruments = instrument_list.content.split(' ')
+  instruments = instruments.drop(7)
+  instruments.each do |instrument|
+    print '#'
+    Instrument.create(name: instrument)
+  end
+end
+print ' }'
+
+# End creating instruments
