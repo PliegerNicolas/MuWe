f22cdd3bd73a463541d3cc1426161d5e4ba55ab8
Nicolas Plieger <plieger05@gmail.com>
2020-03-01 23:39:11 +0000

    Adding comment for precisions in model Event

--- app/models/event.rb
+++ app/models/event.rb
@@ -9,4 +9,5 @@ class Event < ApplicationRecord
   after_validation :geocode, if: :will_save_change_to_address?
 
   enum status: { planned: 0, active: 1, finished: 2 }
+  # .planned(! or ?), .active(! or ?), .finished(! or ?) to update or check the status easily
 end
