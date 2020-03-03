5ef1599d285365cd6d7260526ccec8673ef7f256
Nicolas Plieger <plieger05@gmail.com>
2020-03-03 19:29:20 +0000

    Creating a custom form for user and his profile => working

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   get 'jams', to: 'events#jams' # Main jams page similar to index
   get 'prospect', to: 'posts#prospect' # Main prospect page
 
+  get 'profile/edit', to: 'profiles#edit', as: :edit_profile
+  patch 'profile/:id', to: 'profiles#update', as: :update_profile
   get 'profile/:id', to: 'profiles#profile', as: :profile # current_user profile page
-
   resources :events
+  resources :profiles, only: [:edit]
 end
