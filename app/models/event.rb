2424814f292838bda61bc43b467063144bbdf92d
Nicolas Plieger <plieger05@gmail.com>
2020-03-03 17:07:35 +0000

    Adding photo to event form (functionnal) and updating current_user.profile edit form(s)

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,7 @@ class Event < ApplicationRecord
   has_many :participants, dependent: :destroy
   has_many :comments
   belongs_to :music_style
+  has_one_attached :location_photo
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
