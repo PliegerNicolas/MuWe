5fc87e3169f36f2b507687578e5a6d78c4e7aa73
Nicolas Plieger <plieger05@gmail.com>
2020-03-11 00:42:07 +0000

    Adding status color and adding default participant to event in seed

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,7 +8,7 @@ class Event < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
-  enum status: { planned: 0, active: 1, finished: 2 }
+  enum status: { planned: 0, ongoing: 1, finished: 2 }
   # .planned(! or ?), .active(! or ?), .finished(! or ?) to update or check the status easily
   def accepted_participants
     self.participants.where("participants.status=?", 1) #TODO dynamically get accepted status from participants
