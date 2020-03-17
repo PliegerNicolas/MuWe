f9acc5bf1110606d68bf7e7e89eba582da1363e5
Nicolas Plieger <plieger05@gmail.com>
2020-03-17 06:24:23 +0000

    Rendering the map where the user is without weard zoom and displaying filtered events but all over the map

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,18 +1,17 @@
 import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css';
 
-import { initMapbox } from '../plugins/mapbox'
+import { initMap } from '../plugins/mapbox'
 import { initCards } from '../plugins/cards';
 import { initSelect2 } from '../plugins/init_select2';
 import "../plugins/flatpickr"
 import { addressAutocomplete } from "../plugins/address_autocomplete";
 
 
-initSelect2();
-initMapbox();
-window.initMapbox = initMapbox;
 initCards();
-
+initSelect2();
 $('.select2').select2({ width: '100%' });
 
 addressAutocomplete();
+initMap();
+window.initMap = initMap;
--- app/javascript/plugins/mapbox.js
+++ app/javascript/plugins/mapbox.js
@@ -1,120 +1,62 @@
+import mapboxgl from 'mapbox-gl';
 import { debounce } from 'lodash';
 import axios from 'axios'
-import mapboxgl from 'mapbox-gl';
-
-let map;
-let position = null;
-let events;
-
-const fitBound = () => {
-  let latitudes = events.filter(event => {
-    // console.log(event.longitude);
-    // console.log(event.latitude);
-    return event.latitude
-  });
-  let longitudes = events.filter(event => event.longitude);
-  console.log(latitudes);
-  console.log(longitudes);
-};
 
 const fitMapToMarkers = (map, markers) => { // We'll have to replace markers by position of current_user
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 1500 });
 };
 
-const fetchMarkers =  async () => {
-  const pos = map.getCenter();
-  const res = await axios.get('/search', {
-    params: {
-      latitude: pos.lat,
-      longitude: pos.lng
-    }
-  });
-  return res.data.events;
-}
-
-const bouncedMarkers = debounce(() => {
-  fetchMarkers()
-    .then((response) => {
-      response.forEach((marker) => {
-        // console.log(marker);
-
-        let popup = new mapboxgl.Popup({ offset: 25, maxWidth: '320px' }).setHTML(
-          `<div class="popup-marker">
-            <h3>${marker.title}</h3>
-            <p>${marker.description}</p>
-            <p>Players: ${marker.max_players} | Status: ${marker.status}</p>
-          </div>`
-        );
-
-        let color = (marker.status == 'planned') ? '#44AEE6' : '#6BB382';
-
-        new mapboxgl.Marker({color})
-          .setLngLat([ marker.longitude, marker.latitude ])
-          .setPopup(popup)
-          .addTo(map);
-      });
-    });
-}, 1000);
-
-const initMapbox = () => {
-
+const initMap = () => {
+  let map, initUserPos;
   let sPath = window.location.pathname;
   let sPage = sPath.substring(sPath.lastIndexOf('/') + 1);
 
-  if(sPage == "") {
+  navigator.geolocation.getCurrentPosition(function (position) {
+    initUserPos = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+    }
+    window.currentUserPosition = initUserPos; // Create console command to get user's position
+    console.log("Current user\'s position :", initUserPos); // We've got the initial user position here
 
-    const mapElement = document.getElementById('map');
-    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    if(sPage == '') {
+      const mapElement = document.getElementById('map'); // Get info about map in HTML
+      mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
 
-    if (mapElement) { // only build a map if there's a div#map to inject into
-      map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v10',
-        center: [-9.142685, 38.736946], // hard code lng & lat from Lisbon
-        zoom: 9 // starting zoom
-      });
+      if (mapElement) { // Initalise first view of mapbox map
+        map = new mapboxgl.Map({
+          container: 'map',
+          style: 'mapbox://styles/mapbox/streets-v10',
+          center: [initUserPos.lng, initUserPos.lat],
+          zoom: 11.89
+        })
+      }
 
-      // const markers = JSON.parse(mapElement.dataset.markers);
-      // markers.forEach((marker) => {
-      //   new mapboxgl.Marker()
-      //     .setLngLat([ marker.lng, marker.lat ])
-      //     .addTo(map);
-      // });
-
-
-      const geolocate = new mapboxgl.GeolocateControl({
+      const geolocate = new mapboxgl.GeolocateControl({ // Geolocation settings
         positionOptions: {
           enableHighAccuracy: true
         },
         trackUserLocation: true,
         showAccuracyCircle: false
       });
-      map.addControl(geolocate);
 
-      map.on('load', () => {
-
-        // const events = getEventsNear(geolocate).then((res) => {
-        //   console.log(res);
-        // });
+      map.addControl(geolocate); // Start geolocating
+      map.on('load', () => { // Trigger geolocation client side on load
         geolocate.trigger();
+      })
 
-        geolocate.on('geolocate', (pos) => {
-          console.log(pos.coords.latitude);
-          console.log(pos.coords.longitude);
-          position = pos.coords;
-        });
+      // Execute the rest => what happens with the markers
+
+      const markers = JSON.parse(mapElement.dataset.markers);
+      markers.forEach((marker) => {
+        new mapboxgl.Marker()
+          .setLngLat([ marker.lng, marker.lat ])
+          .addTo(map);
       });
-
-
-      map.on('render', () => {
-        bouncedMarkers();
-      });
-
-      // fitMapToMarkers(map, markers);
+      fitMapToMarkers(map, markers);
     }
-  }
-};
+  });
+}
 
-export { initMapbox, map };
+export { initMap };
