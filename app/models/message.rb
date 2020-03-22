97f9324fdf692adfda42341e38a95129255acedc
Nicolas Plieger <plieger05@gmail.com>
2020-03-22 03:57:44 +0000

    Verify if message is not empty and destroy events if user destroyed

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,6 +4,8 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :event
 
+  validates :message, presence: true
+
   def broadcast_message
     ActionCable.server.broadcast("event_chat_#{event.id}", { message_partial: ApplicationController.renderer.render(partial: "messages/message", locals: { message: self, user_is_message_author: false }), current_user_id: user.id })
   end
