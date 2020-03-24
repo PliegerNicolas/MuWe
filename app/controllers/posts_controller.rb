932f941f6dddc8645f911960b8b49999231a5dc1
Nicolas Plieger <plieger05@gmail.com>
2020-03-24 16:40:40 +0000

    Create post functionnal but not ajax

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,20 @@
 class PostsController < ApplicationController
   def create
-    @profile = Profile.find(params[:profile_id])
-      raise
+    @profile = Profile.find(current_user.id)
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    authorize @post
+    if @post.save
+      respond_to do |format|
+        format.html { redirect_to profile_path(@profile.id) }
+        format.js
+      end
+    else
+      respond_to do |format|
+        format.html { render 'profiles/profile' }
+        format.js
+      end
+    end
   end
 
   def edit
