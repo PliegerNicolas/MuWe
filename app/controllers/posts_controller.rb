dff96c49aa9e0d7b1fe9457c4cb29ed87a1e9a22
Nicolas Plieger <plieger05@gmail.com>
2020-03-25 00:18:42 +0000

    Youtube working, soundcloud not for the moment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,7 @@ class PostsController < ApplicationController
     @profile = Profile.find(current_user.id)
     @post = Post.new(post_params)
     @post.user_id = current_user.id
+    add_links_to_references(@post.description)
     authorize @post
     if @post.save
       respond_to do |format|
@@ -28,6 +29,15 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:user_id, :description, :reference)
+    params.require(:post).permit(:user_id, :description, :references)
+  end
+
+  def add_links_to_references(text)
+    youtube_regex = %r{(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.[^\s]+}
+    soundcloud_regex = %r{(http(s)?:\/\/)?((w){3}.)?(soundcloud|snd)(\.com|\.sc)?\/.[^\s]+}
+    regexes = [youtube_regex, soundcloud_regex]
+    regexes.each do |regex|
+      @post.references << text.match(regex)[0] if text.match(regex)
+    end
   end
 end
--- db/migrate/20200324161337_adding_reference_to_post_model.rb
+++
@@ -1,5 +0,0 @@
-class AddingReferenceToPostModel < ActiveRecord::Migration[5.2]
-  def change
-    add_column :posts, :reference, :string
-  end
-end
