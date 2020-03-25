6e3c4973c4343f4639d567290e6a7c3da5d4e877
Nicolas Plieger <plieger05@gmail.com>
2020-03-25 04:38:22 +0000

    Arranging comments

--- app/helpers/embed_link_helper.rb
+++ app/helpers/embed_link_helper.rb
@@ -2,7 +2,6 @@ module EmbedLinkHelper
   def embed_link(post)
     if post.references.first
       youtube_regex = %r{(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.[^\s]+}
-      # soundcloud_regex = %r{(http(s)?:\/\/)?((w){3}.)?(soundcloud|snd)(\.com|\.sc)?\/.[^\s]+} SOUNDCLOUD IS AN ANOYING API
       url = post.references.first
       if url =~ youtube_regex
         youtube_id = url.split('v=')[1]
@@ -11,3 +10,6 @@ module EmbedLinkHelper
     end
   end
 end
+
+# post.description.sub!(youtube_regex, (link_to url))
+      # soundcloud_regex = %r{(http(s)?:\/\/)?((w){3}.)?(soundcloud|snd)(\.com|\.sc)?\/.[^\s]+} SOUNDCLOUD IS AN ANOYING API
