f139e14c33f94f933a84e1ff579eef87feec6a7a
Nicolas Plieger <plieger05@gmail.com>
2020-03-26 07:56:02 +0000

    Having all events you participate in and all events you've created. BE CAREFUL NO SECURE PUNDIT YET

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,7 @@
 class DashboardsController < ApplicationController
   def dashboard
-    @events = Event.where(user_id: current_user.id)
-    authorize @events
+    @events_created = Event.where(user_id: current_user.id)
+    @events_participating = Participant.where(user: current_user.id).map(&:event)
+    authorize @events_created
   end
 end
