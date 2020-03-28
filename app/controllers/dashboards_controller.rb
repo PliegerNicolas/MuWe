476a37a92bd2a1b6742155fda22d184fa67b234a
Nicolas Plieger <plieger05@gmail.com>
2020-03-28 06:02:46 +0000

    Ordering a bit better

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,7 @@
 class DashboardsController < ApplicationController
   def dashboard
-    @events_created = current_user.events.order(created_at: :desc)
-    @events_participating = Event.where.not(user: current_user).joins(:participants).where("participants.user_id = ?", current_user.id).order(start_date: :desc)
+    @events_created = current_user.events.where.not(status: nil, address: nil).order(:status, created_at: :desc)
+    @events_participating = Event.where.not(user: current_user).joins(:participants).where("participants.user_id = ?", current_user.id)
     authorize @events_created
   end
 end
