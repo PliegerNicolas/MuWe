34dca7784fdd9ddfb13ddc538ae92362e6f60ef9
Nicolas Plieger <plieger05@gmail.com>
2020-03-28 11:01:34 +0000

    Autocomplete only cities on filter_form

--- app/javascript/plugins/address_autocomplete.js
+++ app/javascript/plugins/address_autocomplete.js
@@ -1,7 +1,10 @@
 function addressAutocomplete() {
   const addressInput = document.getElementById('event_address');
+  const filterAddressInput = document.getElementById('filter_city');
   if (addressInput) {
     const autocomplete = new google.maps.places.Autocomplete(addressInput);
+  } else if (filterAddressInput) {
+    const autocomplete = new google.maps.places.Autocomplete(filterAddressInput, { types: ['(cities)'] })
   }
 }
 
