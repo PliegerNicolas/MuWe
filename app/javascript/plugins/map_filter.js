bb845510688a501867d24ed73d3a5244f841de6c
Sofia Mogas <sofia.mogas@gmail.com>
2020-03-28 16:34:00 +0000

    toggle-chat

--- app/javascript/plugins/map_filter.js
+++ app/javascript/plugins/map_filter.js
@@ -5,40 +5,46 @@ import {
 import axios from 'axios'
 
 const map_filter = () => {
-  document.querySelector("#filter_button").addEventListener("click", function () {
-    console.log("Filtering ...");
-    bouncedMarkers();
-  })
+  const filterButton = document.querySelector("#filter_button");
+  if (filterButton) {
+    filterButton.addEventListener("click", function () {
+      console.log("Filtering ...");
+      bouncedMarkers();
+    })
+  }
 }
 
 const city_filter = () => {
-  document.querySelector("#filter_button").addEventListener("click", function () {
-    console.log("Filtering ...");
+  const filterButton = document.querySelector("#filter_button");
+  if (filterButton) {
+    document.querySelector("#filter_button").addEventListener("click", function () {
+      console.log("Filtering ...");
 
-    const pos = map.getCenter();
-    const { _sw, _ne } = map.getBounds();
+      const pos = map.getCenter();
+      const { _sw, _ne } = map.getBounds();
 
-    const request = axios.get('/search', {
-      params: {
-        latitude: pos.lat,
-        longitude: pos.lng,
-        max_lat: _ne.lat,
-        min_lat: _sw.lat,
-        max_lng: _ne.lng,
-        min_lng: _sw.lng,
-        city: document.querySelector("#filter_city").value,
-        periode: document.querySelector("#filter_periode").value,
-        start_time: document.querySelector("#filter_start_time").value,
-        end_time: document.querySelector("#filter_end_time").value,
-        max_players: document.querySelector("#filter_max_players").value,
-        status: document.querySelector("#filter_status").value
-      }
-    });
-    request.then(function(res) {
-      flyToCity(res.data.city_coords, res.data.map_box_limit);
+      const request = axios.get('/search', {
+        params: {
+          latitude: pos.lat,
+          longitude: pos.lng,
+          max_lat: _ne.lat,
+          min_lat: _sw.lat,
+          max_lng: _ne.lng,
+          min_lng: _sw.lng,
+          city: document.querySelector("#filter_city").value,
+          periode: document.querySelector("#filter_periode").value,
+          start_time: document.querySelector("#filter_start_time").value,
+          end_time: document.querySelector("#filter_end_time").value,
+          max_players: document.querySelector("#filter_max_players").value,
+          status: document.querySelector("#filter_status").value
+        }
+      });
+      request.then(function(res) {
+        flyToCity(res.data.city_coords, res.data.map_box_limit);
+      })
+      bouncedMarkers();
     })
-    bouncedMarkers();
-  })
+  }
 }
 
 const flyToCity = (city_coords, map_box_limit) => {
