8c364a095717699ab54a4333efe8d364bb2b3902
Nicolas Plieger <plieger05@gmail.com>
2020-03-28 06:23:11 +0000

    Finishing design of dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,6 @@
 class DashboardsController < ApplicationController
   def dashboard
-    @events_created = current_user.events.where.not(status: nil, address: nil).order(:status, created_at: :desc)
+    @events_created = current_user.events.order(:status, created_at: :desc) # where.not(status: nil, address: nil)    security needed for later when seeds gone
     @events_participating = Event.where.not(user: current_user).joins(:participants).where("participants.user_id = ?", current_user.id)
     authorize @events_created
   end
