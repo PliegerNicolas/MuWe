8e4c739798baa2b4c7f17e22449f40f1047b23dc
Nuno Martins <nuno@alvesmartins.eu>
2020-04-01 00:56:48 +0100

    Add ajax way to add instruments on profile page

--- app/controllers/instrument_users_controller.rb
+++ app/controllers/instrument_users_controller.rb
@@ -1,2 +1,8 @@
 class InstrumentUsersController < ApplicationController
+    def create
+        skip_authorization
+        InstrumentUser.create! user_id: params[:profile_id], instrument_id: params[:instrument_id]
+        # @profile = Profile.find(params[:profile_id])
+    end
+
 end
