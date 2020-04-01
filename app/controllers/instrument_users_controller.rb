674d30258f03674b54fd2703f3819e6139d556e4
Nuno Martins <nuno@alvesmartins.eu>
2020-04-01 01:38:09 +0100

    Allow user to remove instruments from profile page

--- app/controllers/instrument_users_controller.rb
+++ app/controllers/instrument_users_controller.rb
@@ -2,7 +2,10 @@ class InstrumentUsersController < ApplicationController
     def create
         skip_authorization
         InstrumentUser.create! user_id: params[:profile_id], instrument_id: params[:instrument_id]
-        # @profile = Profile.find(params[:profile_id])
     end
 
+    def destroy
+        skip_authorization
+        InstrumentUser.where(user_id: params[:profile_id], instrument_id: params[:id]).destroy_all
+    end
 end
